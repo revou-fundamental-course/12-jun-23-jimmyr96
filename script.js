2502d31c2686bc3629c4828912cc052ff0767ec8
jimmyr96 <137082141+jimmyr96@users.noreply.github.com>
2023-06-22 10:13:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function addNum(){
+    var Celcius = parseFloat(document.getElementById("value1").value);
+    var Fahreinheit = document.getElementById("answer");
+    Fahreinheit.value = (Celcius * 1.8) + 32;
+}
+function myFunction() {
+    document.getElementById("delete").reset();
+  }
